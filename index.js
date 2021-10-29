dc1c9f0dc1deaa94e88531628e361b26909f2673
TahminaChowdhury <tahmina10st@gmail.com>
2021-10-29 09:20:09 +0600

    commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
 const app = express();
 const cors =require("cors");
 require('dotenv').config();
@@ -8,6 +9,26 @@ const port =process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ckcl0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try{
+        await client.connect();
+
+        const database = client.db("guesterra");
+        const usersCollection = database.collection("users");
+
+        
+    }
+    finally{
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
